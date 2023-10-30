083057e4bba6540d00c0fc71b8fe7b5cf97fe269
deltam3 <practicect@proton.me>
2023-10-30 21:09:12 +0900

    difference in time

--- Easy/2_CountdownTimer/JSversion/main.js
+++ Easy/2_CountdownTimer/JSversion/main.js
@@ -21,20 +21,22 @@ const startHandler = (e) => {
   minuteInput.classList.toggle("hide");
   secondInput.classList.toggle("hide");
 
+  console.log(hourInputValue);
   let x = setInterval(() => {
     // Get today's date and time
     var now = new Date().getTime();
-    var countDownDate = new Date("Oct 30, 2023 01:00:00").getTime();
+    // var countDownDate = new Date("Oct 30, 2023 01:00:00").getTime();
+    let countDownDate = new Date();
 
-    // Find the distance between now and the count down date
-    let distance = countDownDate - now;
+    // Find the difference between now and the count down date
+    let difference = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
     let hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
     // document.getElementById("demo").innerHTML =
@@ -45,7 +47,7 @@ const startHandler = (e) => {
     document.querySelector(".second-output").textContent = seconds;
 
     // If the count down is finished,
-    if (distance < 0) {
+    if (difference < 0) {
       // clearInterval(x);
     }
   }, 1000);
