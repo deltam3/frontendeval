7511e4714f19320b6ae7d5ba6de9cf6ab776c3c0
deltam3 <practicect@proton.me>
2023-11-07 21:44:33 +0900

    initial setup

--- Easy/5_MortgageCalculator/JSversion/main.js
+++ Easy/5_MortgageCalculator/JSversion/main.js
@@ -1,5 +1,3 @@
-// console.log(loanAmount);
-
 const btnEl = document.querySelector(".btn");
 btnEl.addEventListener("click", (e) => {
   e.preventDefault();
@@ -11,12 +9,6 @@ btnEl.addEventListener("click", (e) => {
   let r = +interestRate.value;
   let n = +loanLength.value;
 
-  // const firstResult = (monthlyRate * (1 + r)) ** totalMonths;
-  // const secondResult = (1 + monthlyRate) ** totalMonths - 1;
-
-  // console.log(secondResult);
-  // const result = P * (firstResult / secondResult);
-
   const result =
     P *
     ((monthlyRate * Math.pow(1 + monthlyRate, totalMonths)) /
@@ -27,9 +19,3 @@ btnEl.addEventListener("click", (e) => {
     ".result"
   ).textContent = `Your monthly mortgage payment will be $${parseInt(result)}`;
 });
-
-// console.log(result);
-// function calculator(e) {
-
-//   console.log("log");
-// }
