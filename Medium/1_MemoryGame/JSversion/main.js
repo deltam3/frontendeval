853acd45dff94e24ae886afb4608ca637b6f3799
deltam3 <practicect@proton.me>
2023-11-09 21:37:33 +0900

    basic card reveal logic implemented

--- Medium/1_MemoryGame/JSversion/main.js
+++ Medium/1_MemoryGame/JSversion/main.js
@@ -1,6 +1,6 @@
 let revealedCardsCount = 0;
-let revealedCardOneNumber;
-let revealedCardTwoNumber;
+let revealedCardOne;
+let revealedCardTwo;
 
 let gameItems = document.querySelectorAll(".game-item");
 
@@ -9,11 +9,26 @@ gameItems.forEach((gameItem) => {
 });
 
 function handleClick(e) {
-  if (revealedCardsCount === 2) {
-    if (revealedCardOneNumber === revealedCardTwoNumber) {
-      console.log("winner");
-    } else {
-      setTimeout();
+  if (revealedCardsCount === 1) {
+    // clicking for the second time
+    e.target.classList.toggle("clicked");
+    revealedCardTwo = e.target;
+    revealedCardsCount++;
+
+    if (revealedCardOne.textContent === revealedCardTwo.textContent) {
+      document.body.classList.add("click-off");
+
+      setTimeout(() => {
+        revealedCardOne.classList.add("hidden");
+        revealedCardTwo.classList.add("hidden");
+        revealedCardsCount = 0;
+        document.body.classList.remove("click-off");
+      }, 3000);
     }
+  } else if (revealedCardsCount === 0) {
+    // clicking for the first time
+    e.target.classList.toggle("clicked");
+    revealedCardOne = e.target;
+    revealedCardsCount++;
   }
 }
