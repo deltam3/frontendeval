87bcee00ae7e4dc34d4f32e28c03ddfab8a7829d
deltam3 <practicect@proton.me>
2023-11-10 21:21:09 +0900

    game reset functionality finished & small bugs fixed

--- Medium/1_MemoryGame/JSversion/main.js
+++ Medium/1_MemoryGame/JSversion/main.js
@@ -1,6 +1,7 @@
 let revealedCardsCount = 0;
 let revealedCardOne;
 let revealedCardTwo;
+let gameItemNumberLeft = 36;
 
 let gameItems = document.querySelectorAll(".game-item");
 
@@ -23,6 +24,15 @@ function handleClick(e) {
         revealedCardTwo.classList.add("hidden");
         revealedCardsCount = 0;
         document.body.classList.remove("click-off");
+        gameItemNumberLeft -= 2;
+        checkGameItem();
+      }, 3000);
+    } else {
+      setTimeout(() => {
+        revealedCardOne.classList.remove("clicked");
+        revealedCardTwo.classList.remove("clicked");
+        revealedCardsCount = 0;
+        document.body.classList.remove("click-off");
       }, 3000);
     }
   } else if (revealedCardsCount === 0) {
@@ -32,3 +42,25 @@ function handleClick(e) {
     revealedCardsCount++;
   }
 }
+
+function checkGameItem() {
+  if (gameItemNumberLeft === 0) {
+    let gameEl = document.querySelector(".game");
+    let modalEl = document.querySelector(".modal");
+    modalEl.classList.toggle("active");
+  }
+}
+
+let playAgainEl = document.querySelector(".playAgain");
+playAgainEl.addEventListener("click", () => {
+  gameItemNumberLeft = 36;
+  let gameItems = document.querySelectorAll(".game-item");
+  let modalEl = document.querySelector(".modal");
+  gameItems.forEach((gameItem) => {
+    gameItem.classList.remove("hidden");
+    gameItem.classList.remove("clicked");
+    gameItem.classList.add("game-item");
+  });
+  modalEl.classList.remove("active");
+  modalEl.classList.add("displayNone");
+});
