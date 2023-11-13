a210653fa497a9f11e0aac30cd0a0a751b092079
deltam3 <practicect@proton.me>
2023-11-13 20:37:28 +0900

    history basic functinality implemented

--- Medium/2_UndoableCounter/JSversion/js/main.js
+++ Medium/2_UndoableCounter/JSversion/js/main.js
@@ -1,14 +1,21 @@
-// console.log("main");
+let beforeNumber = 0;
+let newNumber;
+let historyArray = [];
 
 let counterNumberEl = document.querySelector(".counter-number");
-let currentCounterNumber =
-  +document.querySelector(".counter-number").textContent;
-let newCounterNumber;
-
 let btns = document.querySelectorAll(".btn");
 
 const updateCount = (number) => {
-  currentCounterNumber += number;
-  counterNumberEl.textContent = currentCounterNumber;
-  console.log(currentCounterNumber);
+  counterNumberEl.textContent = beforeNumber += number;
+  newNumber = counterNumberEl.textContent;
+  updateHistory(beforeNumber, number, newNumber);
+};
+
+const updateHistory = (beforeNumber, inputNumber, newNumber) => {
+  let newHistory = document.createElement("div");
+  newHistory.innerHTML = `<span>${inputNumber}</span> <span>(${
+    beforeNumber - inputNumber
+  } -> ${newNumber})</span>`;
+  let historyEl = document.querySelector(".history");
+  historyEl.append(newHistory);
 };
