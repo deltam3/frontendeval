90a3fdf600fc4d0881c59417a65b9fc614de484d
deltam3 <practicect@proton.me>
2023-11-20 20:31:56 +0900

    fetching partial array items feature

--- Medium/3_ShoppingList/JSversion/js/main.js
+++ Medium/3_ShoppingList/JSversion/js/main.js
@@ -1,3 +1,35 @@
+// const shoppingItems = [{}];
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   console.log(e.target.value);
+// };
+
+// const handleInput = (e) => {
+//   e.preventDefault();
+//   console.log(e.target.value);
+// };
+
+const inputEl = document.querySelector(".textInput");
+inputEl.addEventListener("change", (e) => {
+  e.preventDefault();
+
+  const fetchPartialItems = async () => {
+    let promise = await fetch(
+      `https://api.frontendeval.com/fake/food/${e.target.value}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        return json;
+      });
+    return promise;
+  };
+  let returnedItems = fetchPartialItems();
+  console.log(returnedItems);
+});
+
 const shoppingListEl = document.querySelector(".shopping-list");
 const handleDeleteBtn = (e) => {
   let target = e.target;
