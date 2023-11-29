2f60ffe12c776a3e6add4d7b7cc7cb24684c0e22
deltam3 <practicect@proton.me>
2023-11-29 22:46:30 +0900

    dice roll functionality implemented

--- Medium/5_RollingDice/JSversion/js/main.js
+++ Medium/5_RollingDice/JSversion/js/main.js
@@ -8,7 +8,47 @@ inputEl.addEventListener("change", (e) => {
     for (let i = 1; i <= e.target.value; i++) {
       let diceEl = document.createElement("span");
       diceEl.classList.add("dice");
+      diceEl.classList.add("before");
       dicesEl.append(diceEl);
     }
   }
+});
+
+let rollBtn = document.querySelector(".btn--full");
+rollBtn.addEventListener("click", () => {
+  let allDicesEl = document.querySelectorAll(".dice");
+
+  setTimeout(() => {
+    item.classList.remove("before");
+    item.classList.add("rolling");
+  }, 1000);
+
+  allDicesEl.forEach((item) => {
+    item.classList.remove("rolling");
+
+    function randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    let randomNumber = randomIntFromInterval(1, 6);
+    switch (randomNumber) {
+      case 1:
+        item.classList.add("result1");
+        return;
+      case 2:
+        item.classList.add("result2");
+        return;
+      case 3:
+        item.classList.add("result3");
+        return;
+      case 4:
+        item.classList.add("result4");
+        return;
+      case 5:
+        item.classList.add("result5");
+        return;
+      case 6:
+        item.classList.add("result6");
+        return;
+    }
+  });
 });
