2dad7b18680b59dd5fd8e20f60d625e050f4e4df
deltam3 <practicect@proton.me>
2023-12-09 14:20:41 +0900

    number logic completed

--- Medium/8_2FA/JSversion/js/main.js
+++ Medium/8_2FA/JSversion/js/main.js
@@ -1,1 +1,29 @@
-console.log("test");
+const numberInputs = document.querySelectorAll(".input-number");
+console.log(numberInputs);
+numberInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    if (input.value.length < 1) {
+      if (!input.classList.contains("input-number-fourth")) {
+        input.nextSibling.nextSibling.focus();
+      }
+      if (input.classList.contains("input-number-fourth")) {
+        return;
+      }
+    }
+
+    if (
+      input.value.length === 1 &&
+      !input.classList.contains("input-number-fourth")
+    ) {
+      input.value = e.target.value;
+      input.nextSibling.nextSibling.focus();
+    } else if (
+      input.value.length >= 1 &&
+      input.classList.contains("input-number-fourth")
+    ) {
+      input.value = e.target.value;
+      let submitBtn = document.querySelector(".btn");
+      submitBtn.focus();
+    }
+  });
+});
