56da1f3284b6389de62c8c619868ae66a394a53f
deltam3 <practicect@proton.me>
2023-12-25 17:23:16 +0900

    completed the countdown functionality

--- Easy/2_CountdownTimer/react_version/countdown/src/components/AfterStart.jsx
+++ Easy/2_CountdownTimer/react_version/countdown/src/components/AfterStart.jsx
@@ -9,15 +9,40 @@ function AfterStart({ timer, setTimer, handleActive }) {
 
   useEffect(() => {
     let countdown = setInterval(() => {
+      // sec이 0이 아닐때
       if (timer.sec !== 0) {
         setTimer((timer) => {
           return { ...timer, sec: timer.sec - 1 };
         });
       }
+
+      // sec이 0일때
       if (timer.sec === 0) {
-        setTimer((timer) => {
-          return { ...timer, sec: 59 };
-        });
+        // min이 0 일때
+        if (timer.min === 0) {
+          // hour이 0이 아니면 줄이고
+          if (timer.hour !== 0) {
+            setTimer((timer) => {
+              return { ...timer, hour: timer.hour - 1, min: 59, sec: 59 };
+            });
+          }
+          // Hour이 1이면 59분
+          if (timer.hour === 1) {
+            setTimer((timer) => {
+              return { ...timer, hour: 0, min: 59, sec: 59 };
+            });
+          }
+        }
+        // min이 0이 아닐때
+        if (timer.min !== 0) {
+          setTimer((timer) => {
+            return { ...timer, min: timer.min - 1, sec: 59 };
+          });
+        }
+        // turn the timer off when the timer is finished
+        if (timer.hour === 0 && timer.min === 0) {
+          handleActive();
+        }
       }
     }, 1000);
 
@@ -35,7 +60,7 @@ function AfterStart({ timer, setTimer, handleActive }) {
       </div>
       <div>
         <Button primary>Pause</Button>
-        <Button primary onClick={(e) => handleActive(e)}>
+        <Button primary onClick={() => handleActive()}>
           Reset
         </Button>
       </div>
