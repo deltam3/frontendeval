2be31ae8364a5312be5237d44a0fed295d8d4298
deltam3 <practicect@proton.me>
2023-12-26 21:06:34 +0900

    implemented add timer functionality

--- Easy/2_CountdownTimer/react_version/countdown/src/App.js
+++ Easy/2_CountdownTimer/react_version/countdown/src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import TimerList from "./components/TimerList.jsx";
 
-// const initialTimer = [
-//   { id: 1, hour: "HH", min: "MM", sec: "SS", isActive: false },
-//   { id: 2, hour: "HH", min: "MM", sec: "SS", isActive: false },
-// ];
 const initialTimers = [{ id: 1 }, { id: 2 }];
 
 function App() {
   const [timers, setTimers] = useState(initialTimers);
+  const currentId = useRef(2);
+  const handleAddClick = () => {
+    const result = [...timers, { id: currentId.current + 1 }];
+    setTimers(result);
+  };
+
   return (
     <main className="main">
       <section className="section-timer">
         <h1 className="heading-primary center-text">Countdown Timer</h1>
         <TimerList timers={timers} setTimers={setTimers}></TimerList>
+        <button onClick={handleAddClick}>Add Timer</button>
       </section>
     </main>
   );
