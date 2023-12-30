185d592aac41a3a5b742d2704fb45522d46fdfb6
deltam3 <practicect@proton.me>
2023-12-30 13:20:27 +0900

    added modal component

--- Easy/4_Modal/react_version/src/App.js
+++ Easy/4_Modal/react_version/src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
+import { useState } from "react";
 
-import Button from "./components/Button.jsx";
+import Modal from "./components/Modal.jsx";
+
+const BUTTON_WRAPPER_STYLES = {
+  position: "relative",
+  zIndex: 1,
+};
+
+const OTHER_CONTENT_STYLES = {
+  position: "relative",
+  zIndex: 2,
+  backgrroundColor: "red",
+  padding: "10px",
+};
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className="main">
       <section class="section-button">
-        <Button primary>Show offer</Button>
+        <div
+          style={BUTTON_WRAPPER_STYLES}
+          onClick={() => console.log("clicked")}
+        >
+          <button onClick={() => setIsOpen(true)}>Open Modal</button>
+          <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+            Show Offer
+          </Modal>
+          <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+        </div>
       </section>
     </main>
   );
