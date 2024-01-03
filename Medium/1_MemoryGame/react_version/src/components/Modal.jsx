03641614b09443f9e2d68ed6e5bca8b1e3cdf3c7
deltam3 <practicect@proton.me>
2024-01-03 15:56:13 +0900

    error commit

--- Medium/1_MemoryGame/react_version/src/components/Modal.jsx
+++ Medium/1_MemoryGame/react_version/src/components/Modal.jsx
@@ -1,3 +1,31 @@
 import ReactDOM from "react-dom";
 import { useEffect } from "react";
 import "./Modal.css";
+
+function Modal({ onClose, children, actionBar }) {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <div>
+      <div onClick={onClose} className="fixed inset-0 bg-gray-300"></div>
+      <div className="fixed inset-40 p-10 bg-white">
+        <div>
+          <Button onClick={onClose}>X</Button>
+        </div>
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector(".modal-containre")
+  );
+}
+
+export default Modal;
