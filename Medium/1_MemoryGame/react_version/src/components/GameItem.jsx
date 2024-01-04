9e946c259bb34c7345bcc4500397fa1e37447e45
deltam3 <practicect@proton.me>
2024-01-04 21:51:26 +0900

    gameitems can be revealed

--- Medium/1_MemoryGame/react_version/src/components/GameItem.jsx
+++ Medium/1_MemoryGame/react_version/src/components/GameItem.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 
+import className from "classnames";
 import "./GameItem.css";
 
 function GameItem({ item }) {
   const [cardRevealed, setCardRevealed] = useState(false);
 
+  const handleClick = () => {
+    setCardRevealed((cardRevealed) => !cardRevealed);
+  };
+
+  const classes = cardRevealed
+    ? "card-item card-revealed-true"
+    : "card-item card-revealed-false";
+
   return (
-    <div className="card-item">
+    <div onClick={handleClick} className={classes}>
       <div>
         <h1>{item.number}</h1>
       </div>
