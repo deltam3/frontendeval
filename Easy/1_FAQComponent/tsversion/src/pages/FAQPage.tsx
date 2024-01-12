b4118ffca2e58b646933c680c7b4908b3e86ec80
deltam3 <practicect@proton.me>
2024-01-12 09:47:19 +0900

    created page component

---
+++ Easy/1_FAQComponent/tsversion/src/pages/FAQPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const FAQPage = (props: Props) => {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+    </div>
+  );
+};
+
+export default FAQPage;
