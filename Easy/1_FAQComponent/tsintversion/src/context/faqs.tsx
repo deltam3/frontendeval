e4d7787a791ed1f7eb466463ee4f13b5624f0a93
deltam3 <practicect@proton.me>
2024-01-18 09:17:26 +0900

    setup basic usereducer in the faqscontext

--- Easy/1_FAQComponent/tsintversion/src/context/faqs.tsx
+++ Easy/1_FAQComponent/tsintversion/src/context/faqs.tsx
@@ -1,9 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useReducer } from "react";
 
-const FaqsContext = createContext();
+const FaqsContext = createContext(null);
+const FaqDispatchContext = createContext(null);
 
 function Provider({ children }) {
-  const [faqs, setFaqs] = useState([]);
+  // const [faqs, setFaqs] = useState([]);
+  const [faqs, dispatch] = useReducer(faqsReducer, initialFaqs);
   const number = 5;
 
   const valueToShare = {
@@ -12,9 +14,43 @@ function Provider({ children }) {
   };
 
   return (
-    <FaqsContext.Provider value={valueToShare}>{children}</FaqsContext.Provider>
+    <FaqsContext.Provider value={valueToShare}>
+      <FaqDispatchContext.Provider value={dispatch}>
+        {children}
+      </FaqDispatchContext.Provider>
+    </FaqsContext.Provider>
   );
 }
+
+function faqsReducer(faqs, action) {
+  switch (action.type) {
+    case "clicked": {
+      return undefined;
+    }
+  }
+}
+
+const initialFaqs = [
+  {
+    id: 0,
+    question: "How many bones does a cat have?",
+    answer: "A cat has 230 bones - 6 more than a human",
+    isOpen: true,
+  },
+  {
+    id: 1,
+    question: "How much do cats sleep?",
+    answer: "The average cat sleeps 12-16 hours per day",
+    isOpen: false,
+  },
+  {
+    id: 2,
+    question: "How long do cats live",
+    answer:
+      "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    isOpen: false,
+  },
+];
 
 export { Provider };
 export default FaqsContext;
