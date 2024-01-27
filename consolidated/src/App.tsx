33fabe0d1726b7279c6a6850b27a4890836f03c3
deltam3 <practicect@proton.me>
2024-01-27 20:39:57 +0900

    lazy load Easy, Medium, Hard Components

--- consolidated/src/App.tsx
+++ consolidated/src/App.tsx
@@ -4,9 +4,19 @@ import React from "react";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Easy from "./pages/Easy/Easy";
-import Medium from "./pages/Medium/Medium";
-import Hard from "./pages/Hard/Hard";
+// import Easy from "./pages/Easy/Easy";
+// import Medium from "./pages/Medium/Medium";
+// import Hard from "./pages/Hard/Hard";
+
+const Easy = React.lazy(
+  () => import(/* webpackChunkName: "easy" */ "./pages/Easy/Easy")
+);
+const Medium = React.lazy(
+  () => import(/* webpackChunkName: "medium*/ "./pages/Medium/Medium")
+);
+const Hard = React.lazy(
+  () => import(/* webpackChunkName : "hard"*/ "./pages/Hard/Hard")
+);
 
 type Props = {};
 
