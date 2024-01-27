e56beda15a54479e7b7ab984e83356d61200620f
deltam3 <practicect@proton.me>
2024-01-27 14:25:35 +0900

    basic styling and layout for the header component and the home component

--- consolidated/src/components/Header.tsx
+++ consolidated/src/components/Header.tsx
@@ -1,19 +1,40 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
+  const HeaderBox = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem 2rem;
+    gap: 2rem;
+  `;
+  const UlEl = styled.ul`
+    display: flex;
+    gap: 2rem;
+    > a {
+      text-decoration: none;
+      color: #555;
+    }
+
+    > a:hover {
+      text-decoration: underline;
+    }
+  `;
+
   return (
-    <div>
+    <HeaderBox>
       <div>
         <div>
-          <div>
+          <UlEl>
             <Link to="/">Home</Link>
             <Link to="/Easy">Easy Difficulty Projects </Link>
             <Link to="/Medium">Medium Difficulty Projects </Link>
             <Link to="/Hard">Hard Difficulty Projects </Link>
-          </div>
+          </UlEl>
         </div>
       </div>
-    </div>
+    </HeaderBox>
   );
 };
 
--- consolidated/src/pages/Home.tsx
+++ consolidated/src/pages/Home.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 type Props = {};
 
 const Home = (props: Props) => {
+  const HomeContainer = styled.main`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  `;
   return (
-    <div>
+    <HomeContainer>
       <h2>Home</h2>
       <h2>FrontEnd Eval</h2>
       <p>
         <a href="https://frontendeval.com/">frontendeval.com</a>
       </p>
-    </div>
+    </HomeContainer>
   );
 };
 
