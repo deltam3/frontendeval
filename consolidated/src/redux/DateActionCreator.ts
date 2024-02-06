46702a590b6d30b95c6319ed2cdb8acfda5cd198
deltam3 <practicect@proton.me>
2024-02-06 15:31:19 +0900

    initial DateActionCreator commit

---
+++ consolidated/src/redux/DateActionCreator.ts
@@ -0,0 +1,15 @@
+export const DATE_ACTION = {
+  ADD_DATE: "addDate" as const,
+  DELETE_TODO: "delete" as const,
+};
+
+const DateActionCreator = {
+  addDate: (dateItem: { city: string; time: Date }) => {
+    return { type: DATE_ACTION.ADD_DATE, payload: dateItem };
+  },
+  deleteDate: (dateItem: { id: number }) => {
+    return { type: DATE_ACTION.DELETE_TODO, payload: dateItem };
+  },
+};
+
+export type TodoActionType = ReturnType<typeof DateActionCreator.addDate>;
