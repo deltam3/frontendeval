f6ea259447672257a0a68e8627bc5a3f93c512bf
deltam3 <practicect@proton.me>
2024-02-07 12:17:06 +0900

    displaed date data from redux store to Easy container component

--- consolidated/src/pages/Easy/Easy.tsx
+++ consolidated/src/pages/Easy/Easy.tsx
@@ -3,14 +3,27 @@ import FAQPage from "./projects/FAQ/FAQPage";
 import CounterPage from "./projects/Counter/CounterPage";
 import { ProjectItemContainer } from "../../components/ProjectItemContainer";
 
+import DateActionCreator from "../../redux/DateActionCreator";
+import { UnknownAction, Dispatch } from "redux";
+import { connect } from "react-redux";
+import { DateStatesType, DateItemType } from "../../redux/DateReducer";
+
 type Props = {};
 
-function Easy({}: Props) {
+function Easy(props: Props) {
+  const result = props.dateList.map((item) => {
+    return (
+      <p key={item.city}>
+        {item.city} {item.date.toDateString()}
+      </p>
+    );
+  });
   return (
     <div>
       <h1 style={{ textAlign: "center", paddingBottom: "2.4rem" }}>
         Easy Difficulty Projects
       </h1>
+      <div style={{ textAlign: "center" }}>{result}</div>
       <ProjectItemContainer>
         <FAQPage></FAQPage>
       </ProjectItemContainer>
@@ -21,4 +34,14 @@ function Easy({}: Props) {
   );
 }
 
-export default Easy;
+const mapStateToProps = (state: DateStatesType) => ({
+  dateList: state.dateList,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<UnknownAction>) => ({
+  addDate: (id: number) => dispatch(DateActionCreator.addDate({ id })),
+  deleteDate: (id: number) => dispatch(DateActionCreator.deleteDate({ id })),
+});
+
+const EasyContainer = connect(mapStateToProps, mapDispatchToProps)(Easy);
+export default EasyContainer;
