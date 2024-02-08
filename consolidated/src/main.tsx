448bf712fba0f6017980448717ed663dd53bece7
deltam3 <practicect@proton.me>
2024-02-08 09:40:15 +0900

    cleaned main.tsx

--- consolidated/src/main.tsx
+++ consolidated/src/main.tsx
@@ -4,16 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 
 import AppStore from "./redux/AppStore";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-// import rootReducer from "./modules";
-
-// const store = configureStore({ reducer: rootReducer });
-// const store = "redux is working";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
     <Provider store={AppStore}>
       <App />
     </Provider>
