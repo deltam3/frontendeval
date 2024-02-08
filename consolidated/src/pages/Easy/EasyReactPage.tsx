3e53fb95adea17b3c9415b49826dd20d7feb4be0
deltam3 <practicect@proton.me>
2024-02-08 10:24:23 +0900

    made version link component available to all easy project pages

--- consolidated/src/pages/Easy/EasyReactPage.tsx
+++ consolidated/src/pages/Easy/EasyReactPage.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { ProjectItemContainer } from "../../components/ProjectItemContainer";
 import FAQPage from "./projects/FAQ/FAQPage";
 import CounterPage from "./projects/Counter/CounterPage";
+
+import { Link } from "react-router-dom";
+
 type Props = {};
 
 const EasyReactPage = (props: Props) => {
   return (
     <div>
+      <div style={{ textAlign: "center" }}>
+        <Link to="/Easy/Vanillajs">Easy Project - VanillaJS Version</Link>
+        <Link to="/Easy/React">Easy Projects - React Version</Link>
+      </div>
       <h3>Easy Projects done in ReactJS</h3>
       <ProjectItemContainer>
         <FAQPage></FAQPage>
--- consolidated/src/pages/Easy/EasyVanillajsPage.tsx
+++ consolidated/src/pages/Easy/EasyVanillajsPage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 type Props = {};
 
 const EasyVanillajsPage = (props: Props) => {
   return (
     <div>
+      <div style={{ textAlign: "center" }}>
+        <Link to="/Easy/Vanillajs">Easy Project - VanillaJS Version</Link>
+        <Link to="/Easy/React">Easy Projects - React Version</Link>
+      </div>
       <h3>Easy Projects done in VanillaJS</h3>
     </div>
   );
