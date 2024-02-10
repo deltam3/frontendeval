02760cd162add9bef3254c91166ff0d2ee1e0adb
deltam3 <practicect@proton.me>
2024-02-10 13:27:15 +0900

    fixed styling on subpage navlink

--- consolidated/src/pages/Easy/EasyReactPage.tsx
+++ consolidated/src/pages/Easy/EasyReactPage.tsx
@@ -2,19 +2,34 @@ import React from "react";
 import { ProjectItemContainer } from "../../components/ProjectItemContainer";
 import FAQPage from "./projects/FAQ/FAQPage";
 import CounterPage from "./projects/Counter/CounterPage";
+import "./../../components/subpage.css";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 type Props = {};
 
 const EasyReactPage = (props: Props) => {
   return (
-    <div>
+    <div className="subpage-container">
       <div style={{ textAlign: "center" }}>
-        <Link to="/Easy/Vanillajs">Easy Project - VanillaJS Version</Link>
-        <Link to="/Easy/React">Easy Projects - React Version</Link>
+        <NavLink
+          to="/Easy/Vanillajs"
+          className={({ isActive }) =>
+            isActive ? "bg-dark btn" : "btn-default btn"
+          }
+        >
+          Easy Project - VanillaJS Version
+        </NavLink>
+        <NavLink
+          to="/Easy/React"
+          className={({ isActive }) =>
+            isActive ? "bg-dark btn" : "btn-default btn"
+          }
+        >
+          Easy Projects - React Version
+        </NavLink>
       </div>
-      <h3 style={{ textAlign: "center" }}>Easy Projects done in ReactJS</h3>
+      <h3 className="subpage-title">Easy Projects done in ReactJS</h3>
       <ProjectItemContainer>
         <FAQPage></FAQPage>
       </ProjectItemContainer>
