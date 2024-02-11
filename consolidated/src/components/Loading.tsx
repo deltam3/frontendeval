e9338e8a907a8133ea43a4fb4d7ad23f18d3da45
deltam3 <practicect@proton.me>
2024-02-11 15:14:57 +0900

    Changed Loading Component's CSS

--- consolidated/src/components/Loading.tsx
+++ consolidated/src/components/Loading.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { ScaleLoader } from "react-spinners";
+import styled from "styled-components";
 
 const Loading = () => {
+  const LoadingContainer = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `;
+
   return (
-    <div>
+    <LoadingContainer>
       <div>
         <div>
           <h3>Loading</h3>
           <ScaleLoader height="40px" width="6px" radius="2px" margin="2px" />
         </div>
       </div>
-    </div>
+    </LoadingContainer>
   );
 };
 
