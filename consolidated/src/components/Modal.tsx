a69a1bf46d586136e375436967f473384d7197e9
deltam3 <practicect@proton.me>
2024-02-16 16:33:49 +0900

    initial modal functionality complete

--- consolidated/src/components/Modal.tsx
+++ consolidated/src/components/Modal.tsx
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom";
 import React, { useState, useEffect } from "react";
 import Button from "./Button";
 
+type PropTypes = {
+  onClose: () => void;
+  actionBar: any;
+  children: any;
+};
+
 function Modal({ onClose, actionBar, children }: PropTypes) {
   useEffect(() => {
     document.body.classList.add("overflow-hidden");
@@ -13,7 +19,18 @@ function Modal({ onClose, actionBar, children }: PropTypes) {
 
   return ReactDOM.createPortal(
     <div>
-      <div>React Portal</div>
+      <div onClick={onClose} className="fixed inset-0 bg-gray-300"></div>
+      <div className="fixed inset-40 p-10 bg-white">
+        <div>
+          <Button onClick={onClose}>Close Modal</Button>
+        </div>
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex justify-center flex-col">
+            <div>{children}</div>
+            <div className="flex justify-center">{actionBar}</div>
+          </div>
+        </div>
+      </div>
     </div>,
     document.querySelector(".modal-container")
   );
--- consolidated/src/pages/Easy/projects/Modal/ModalPage.tsx
+++ consolidated/src/pages/Easy/projects/Modal/ModalPage.tsx
@@ -1,13 +1,180 @@
 import React from "react";
 import Modal from "../../../../components/Modal";
+import { useState, useEffect } from "react";
+import Button from "../../../../components/Button";
 
-type Props = {};
+const ModalPage = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [showResult, setShowResult] = useState<boolean>(false);
 
-const ModalPage = (props: Props) => {
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleClickAction = () => {
+    setShowModal(false);
+    setShowResult(true);
+  };
+
+  const actionBar = (
+    <Button onClick={handleClickAction} primary>
+      Accept Offer
+    </Button>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <div className="flex justify-center align-middle flex-col">
+        <div className="text-center">
+          PRESS "ESC" on Your Keyboard to exit the Modal
+        </div>
+      </div>
+    </Modal>
+  );
+
+  useEffect(() => {
+    function handleEscape(e: KeyboardEvent) {
+      if (e.key === "Escape") {
+        setShowModal(false);
+      }
+    }
+
+    document.addEventListener("keydown", handleEscape);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscape);
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Modal Page</h1>
-      <Modal></Modal>
+      <div className="width-10 margin-auto">
+        <Button onClick={handleClick} primary>
+          Show Modal
+        </Button>
+      </div>
+
+      {showModal && modal}
+      {showResult ? (
+        <p>Offer Accepted</p>
+      ) : (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat
+            elit in est interdum fermentum. Cras vehicula a urna sit amet
+            auctor. Mauris tempor magna a imperdiet posuere. Quisque quis
+            malesuada felis, sit amet commodo mi. Vestibulum finibus suscipit
+            porta. Donec a enim ac eros pellentesque efficitur. In ut ex enim.
+            Cras lacinia quam id urna dapibus, a consequat mauris posuere. Etiam
+            sed erat quis lacus condimentum viverra scelerisque sit amet elit.
+            Mauris eget nunc sagittis, dapibus risus pharetra, placerat neque.
+            Maecenas tristique risus lorem.
+          </p>
+        </>
+      )}
     </div>
   );
 };
