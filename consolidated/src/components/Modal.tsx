f39e967108a85057a4cb315f814657b71806e897
deltam3 <practicect@proton.me>
2024-02-16 10:48:17 +0900

    added Effect to toggle overflow-hidden tailwind css class

--- consolidated/src/components/Modal.tsx
+++ consolidated/src/components/Modal.tsx
@@ -2,4 +2,12 @@ import ReactDOM from "react-dom";
 import React, { useState, useEffect } from "react";
 import Button from "./Button";
 
-function Modal({});
+function Modal({ onClose, actionBar, children }: PropTypes) {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+}
