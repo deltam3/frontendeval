08a34a25e565b2ac07ea625d71b2273c1a56a56c
deltam3 <practicect@proton.me>
2024-02-16 09:59:54 +0900

    created modal page

---
+++ consolidated/src/pages/Easy/projects/Modal/ModalPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const ModalPage = (props: Props) => {
+  return (
+    <div>
+      <h1>Modal Page</h1>
+    </div>
+  );
+};
+
+export default ModalPage;
