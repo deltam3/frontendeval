45894d9c3dc8b8bca9f861c3e442aa4d73869c01
deltam3 <practicect@proton.me>
2024-02-16 17:09:18 +0900

    deleted useless code

--- consolidated/src/pages/Medium/Medium.tsx
+++ consolidated/src/pages/Medium/Medium.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-import { connect } from "react-redux";
-import { DateStatesType, DateItemType } from "../../redux/DateReducer";
 import { Link } from "react-router-dom";
 
-type Props = {};
-
-function Medium({}: Props) {
+function Medium() {
   return (
     <div>
       <h1 style={{ textAlign: "center", paddingBottom: "1.2rem" }}>
@@ -24,39 +19,3 @@ function Medium({}: Props) {
 }
 
 export default Medium;
-
-// import { Link } from "react-router-dom";
-
-// import { connect } from "react-redux";
-// import { DateStatesType, DateItemType } from "../../redux/DateReducer";
-
-// type Props = {};
-
-// function Easy(props: Props) {
-//   const result = props.dateList.map((item) => {
-//     return (
-//       <p key={item.city}>
-//         {item.city} {item.date.toDateString()}
-//       </p>
-//     );
-//   });
-//   return (
-//     <div>
-//       <h1 style={{ textAlign: "center", paddingBottom: "2.4rem" }}>
-//         Easy Difficulty Projects
-//       </h1>
-//       <div style={{ textAlign: "center" }}>
-//         <Link to="/Easy/Vanillajs">Easy Project - VanillaJS Version</Link>
-//         <Link to="/Easy/React">Easy Projects - React Version</Link>
-//       </div>
-//       <div style={{ textAlign: "center" }}>{result}</div>
-//     </div>
-//   );
-// }
-
-// const mapStateToProps = (state: DateStatesType) => ({
-//   dateList: state.dateList,
-// });
-
-// const EasyContainer = connect(mapStateToProps, null)(Easy);
-// export default EasyContainer;
