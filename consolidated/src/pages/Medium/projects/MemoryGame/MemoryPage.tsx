11868cec572c2b2ecbd919186897a9474ec2e926
deltam3 <practicect@proton.me>
2024-02-17 14:36:27 +0900

    initial setup memorypage component

---
+++ consolidated/src/pages/Medium/projects/MemoryGame/MemoryPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const MemoryPage = (props: Props) => {
+  return (
+    <div>
+      <h2>MemoryPage</h2>
+    </div>
+  );
+};
+
+export default MemoryPage;
