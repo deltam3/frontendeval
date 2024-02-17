bac337e8fdac4d318ec1f00a95cdb3a0f874dd70
deltam3 <practicect@proton.me>
2024-02-17 15:50:21 +0900

    setup the logic part for memorypage

--- consolidated/src/pages/Medium/projects/MemoryGame/MemoryPage.tsx
+++ consolidated/src/pages/Medium/projects/MemoryGame/MemoryPage.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../../../components/Button";
+import Modal from "../../../../components/Modal";
 
 type Props = {};
 
 const MemoryPage = (props: Props) => {
+  const [difficulty, setDifficulty] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleDifficultySelect = (number) => {
+    setShowModal(false);
+    setDifficulty(number);
+  };
+
+  const handleStartButton = () => {
+    setShowModal(!showModal);
+  };
+
+  const modal = (
+    <Modal onClose={handleModalClose}>
+      <section>
+        <div>
+          <div>
+            <Button onClick={() => handleDifficultySelect(4)}>Select 4</Button>
+          </div>
+          <div>
+            <Button onClick={() => handleDifficultySelect(6)}>Select 6</Button>
+          </div>
+          <div>
+            <Button onClick={() => handleDifficultySelect(8)}>Select 8</Button>
+          </div>
+        </div>
+      </section>
+    </Modal>
+  );
+
   return (
     <div>
       <h2>MemoryPage</h2>
