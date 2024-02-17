3ec141e3ee7a5ab0bf817928267e2327fa94a63a
deltam3 <practicect@proton.me>
2024-02-17 17:14:41 +0900

    initial gameitem component functionality complete

--- consolidated/src/pages/Medium/projects/MemoryGame/GameItem.tsx
+++ consolidated/src/pages/Medium/projects/MemoryGame/GameItem.tsx
@@ -15,7 +15,29 @@ const GameItem = ({
 }: Props) => {
   const thisId  = item.id;
 
-
+  const handleClick = () => {
+    if (revealedCardCount.current === 0) {
+      const result = items.map((item) => {
+        if (item.id === thisId) {
+          cardItemOne.current = item;
+          return {...item, revealed: true}
+        }
+        return item
+      })
+      revealedCardCount.current = 1;
+      setItems(result)
+    } else if (revealedCardCount.current === 1 {
+      if (cardItemTwo.current === undefined) {
+        const result = items.map((item) => {
+          if (item.id === thisId) {
+            cardItemTwo.current = item;
+            return {...item, revealed: true}
+          }
+          return item 
+        })
+      }
+    })
+  }
 
   return <div>GameItem</div>;
 };
