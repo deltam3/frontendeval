086d80694e50ae3354228a8605d2fcedd012bc19
deltam3 <practicect@proton.me>
2024-02-17 16:26:06 +0900

    created and added styles to Game.css

--- consolidated/src/pages/Medium/projects/MemoryGame/Game.tsx
+++ consolidated/src/pages/Medium/projects/MemoryGame/Game.tsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect, useRef } from "react";
+import "./Game.css";
 
 type Props = {
   difficulty: boolean | number;
 };
 
 const Game = ({ difficulty }: Props) => {
-  return <div>Game</div>;
+  const totalNumberGameItems = difficulty * difficulty;
+  const [gameItems, setGameItems] = useState([]);
+
+  const revealedCardsCount = useRef(0);
+  const cardItemOne = useRef(undefined);
+  const cardItemTwo = useRef(undefined);
+
+  let gameList;
+  if (difficulty === 4) {
+    gameList = "gamelist-easy";
+  } else if (difficulty === 6) {
+    gameList = "gamelist-medium";
+  } else {
+    gameList = "gamelist-hard";
+  }
+
+  // return <div>Game</div>;
+  return (
+    <div>
+      <div className={gameList}></div>
+    </div>
+  );
 };
 
 export default Game;
